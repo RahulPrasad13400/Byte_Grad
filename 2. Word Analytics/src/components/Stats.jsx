09c8e51ba549ba726fb7d039b89a79185dec7dfd
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-03-07 20:02:19 +0530

    completed

--- 2. Word Analytics/src/components/Stats.jsx
+++ 2. Word Analytics/src/components/Stats.jsx
@@ -1,10 +1,12 @@
+import { FB_MAX_CHARS, INSTA_MAX_CHARS } from "../lib/constants"
+
 export default function Stats({numOfCharacters = 0, numOfWords = 0}) {
   return (
     <section className="stats">
       <Stat label="Words"  number={numOfWords-1} />
       <Stat label="Characters" number={numOfCharacters} />
-      <Stat label="Instagram" number={280-numOfCharacters} warning={280-numOfCharacters > 0 ? '' : 'stat__number--limit'} />
-      <Stat label="Facebook" number={2200-numOfCharacters} warning={2200-numOfCharacters > 0 ? '' : 'stat__number--limit'} />
+      <Stat label="Instagram" number={INSTA_MAX_CHARS-numOfCharacters} warning={280-numOfCharacters > 0 ? '' : 'stat__number--limit'} />
+      <Stat label="Facebook" number={FB_MAX_CHARS-numOfCharacters} warning={2200-numOfCharacters > 0 ? '' : 'stat__number--limit'} />
     </section>
   )
 }
